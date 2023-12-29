2b54b092acc3066497c4d5ee92f4d1eba02fdebe
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-30 00:40:59 +0530

    update lazy loading& suspense

--- zevi-client/src/App.tsx
+++ zevi-client/src/App.tsx
@@ -1,13 +1,22 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home.tsx";
-import Products from "./Pages/ProductsFilter/ProductsFilter.tsx"
+const Home = lazy( () => import( "./Pages/Home/Home" ) );
+const ProductsFilter = lazy( () => import( "./Pages/ProductsFilter/ProductsFilter" ) )
 
-const App = () => {
+
+//loading component
+const Loading: React.FC = () => <div>Loading...</div>
+
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/products" element={ <Products/> } />
+        <Route path="/" element={ <Suspense fallback={ <Loading /> }>
+          <Home />
+        </Suspense> } />
+        <Route path="/products" element={ <Suspense fallback={ <Loading /> }>
+          <ProductsFilter />
+        </Suspense> } />
       </Routes>
     </BrowserRouter>
 
