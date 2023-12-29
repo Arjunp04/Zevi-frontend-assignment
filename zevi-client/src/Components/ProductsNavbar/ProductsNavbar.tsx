4e5be5c63ad468866aaeb8893123923d5b0ce0c5
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-29 18:35:57 +0530

    created productsNavbar.tsx

---
+++ zevi-client/src/Components/ProductsNavbar/ProductsNavbar.tsx
@@ -0,0 +1,18 @@
+import "./ProductsNavbar.scss";
+import zevi_logo from "../../images/ZEVI-GG-LogoDesign.png";
+import { BsSearch } from "react-icons/bs";
+
+const ProductsNavbar = () => {
+    return (
+        <div className='products-navbar'>
+            <img src={ zevi_logo } alt="zevi logo " />
+            <form>
+                <input type='text' placeholder='Search your product...'
+                    className='searchbar-input' />
+                <button className='search-icon'><BsSearch size="20" /></button>
+            </form>
+        </div>
+    )
+}
+
+export default ProductsNavbar
