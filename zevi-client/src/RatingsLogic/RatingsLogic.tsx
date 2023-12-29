1b8c34cc498fe18907a9df3ed070006fd03ec261
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-29 19:53:34 +0530

    created Ratingslogic commit

--- zevi-client/src/RatingsLogic/RatingsLogic.tsx
+++ zevi-client/src/RatingsLogic/RatingsLogic.tsx
@@ -1,10 +1,33 @@
+import { AiFillStar, AiOutlineStar } from "react-icons/ai"
 
-const RatingsLogic = () => {
+export const ratingBox = ( rating: number ) => {
   return (
     <div>
-      Ratings Logic
+      { rating >= 1 ? (
+        <AiFillStar color="rgba(255, 192, 48, 1)" />
+      ) : (
+        <AiOutlineStar color="rgb(212, 206, 206)" />
+      ) }
+      { rating >= 2 ? (
+        <AiFillStar color="rgba(255, 192, 48, 1)" />
+      ) : (
+        <AiOutlineStar color="rgb(212, 206, 206)" />
+      ) }
+      { rating >= 3 ? (
+        <AiFillStar color="rgba(255, 192, 48, 1)" />
+      ) : (
+        <AiOutlineStar color="rgb(212, 206, 206)" />
+      ) }
+      { rating >= 4 ? (
+        <AiFillStar color="rgba(255, 192, 48, 1)" />
+      ) : (
+        <AiOutlineStar color="rgb(212, 206, 206)" />
+      ) }
+      { rating >= 5 ? (
+        <AiFillStar color="rgba(255, 192, 48, 1)" />
+      ) : (
+        <AiOutlineStar color="rgb(212, 206, 206)" />
+      ) }
     </div>
-  )
-}
-
-export default RatingsLogic
+  );
+};
