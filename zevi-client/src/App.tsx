b3b28290eba8c4f77a855aed892797ba57d178e1
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-29 22:12:01 +0530

    created productsfilter section

--- zevi-client/src/App.tsx
+++ zevi-client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.tsx";
-import Products from "./Pages/Products/Products.tsx"
+import Products from "./Pages/ProductsFilter/ProductsFilter.tsx"
 
 const App = () => {
   return (
