62c085dcec28c442044dedf5d2864dfc45ad17be
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-29 19:42:19 +0530

    Products add commit

--- zevi-client/src/App.tsx
+++ zevi-client/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./Pages/Home/Home.tsx";
+import Products from "./Pages/Products/Products.tsx"
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/products" element={ <Products/> } />
+      </Routes>
     </BrowserRouter>
-    
+
   )
 }
 
