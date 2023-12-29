ec4cabc9a16adc4f2c3ad1445d1db8757c7ec9d4
Arjunp04 <arjunprajapati4102001@gmail.com>
2023-12-29 18:36:47 +0530

    created Logo component

---
+++ zevi-client/src/Components/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import zevi_logo from "../../images/ZEVI-GG-LogoDesign.png"
+
+const Logo = () => {
+    return (
+        <div className="home_logo">
+            <img src={ zevi_logo } alt="zevi logo " />
+        </div>
+    )
+}
+
+export default Logo
